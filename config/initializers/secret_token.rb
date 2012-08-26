44254f8ed0d448adfcbf3399d6bc8f99e7912445
Derek <derek.kean@gmail.com>
2012-08-25 21:28:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swagpg::Application.config.secret_token = '536dfd222e4e9b1e36c620f3691fabb9d46c16c84c753eb59ec113a20fdd012f5377a33f0740902bddaba659bc5948eb89156b8cfb213ae84740dcb8695ff251'
