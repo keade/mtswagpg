53edc6c95ccd1b06aacdcac6269a9bbe1ebeaba5
Derek Kean <derek.kean@gmail.com>
2015-09-24 10:21:42 +0800

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,1 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '5gOck6tO3UwHqqRDnIFpQ', 'nJZfGNn7j4tUSMD5Yo3pFMGAzoGNIlnhwP2OrKqZI'
-  provider :github, '198a4e9bc7b00f03ba4e', '81da45e60cd0909834c3aae090234530e8c4adad'
-end
+
